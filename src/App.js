adf6a2ab6af4118ca9c7411d9e4de091f05fb5c2
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-13 12:57:25 -0300

    Testing if gh-pages package is working

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Starting a personal website of Marcelo Maurício using React JS.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
