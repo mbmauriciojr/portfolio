a6958bea1a4f1ef345d0bc1a6008c9a87faaa9c5
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-27 12:15:51 -0300

    Cleanup the files to apply eslint config and resets about css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Starting a personal website of Marcelo Maurício using React JS.
-        </p>
-      </header>
+      <p>
+        Starting a personal website of Marcelo Maurício using React JS.
+      </p>
     </div>
   );
 }
